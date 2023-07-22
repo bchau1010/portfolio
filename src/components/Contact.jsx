949a13c06c650e7a75ec89cbad3de3c034facb1d
bchau1010 <bchau1010@gmail.com>
2023-07-21 21:38:48 -0500

    Initial Commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Title from './Title';
+
+//form tag "action" mean submit data to the address specified
+   //method="Post" will send the form data as http post transaction
+//input tag, field where user can enter data (for form)
+   //type, can be different type such as button, checkbox, etc
+   //place holder: Display what type of input
+function Contact() {
+   return (
+      <div className="flex flex-col mb-10 mx-auto">
+         <div className="flex justify-center items-center">
+            <form
+               action="https://getform.io/f/(customSlugHere)"
+               method="POST"
+               className="flex flex-col w-full md:w-7/12"
+            >
+               <Title>Contact</Title>
+               <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+               />
+               <input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+               />
+               <textarea
+                  name="message"
+                  placeholder="Message"
+                  rows="10"
+                  className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
+               />
+               <button
+                  type="button"
+                  className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white"
+               >
+                  Work With Me
+               </button>
+            </form>
+         </div>
+      </div>
+   )
+}
+
+export default Contact;
